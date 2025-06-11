14eda844b5f50d262cd3c19db4bf3e2cabc94a40
fionas14 <siregarfiona90@gamil.com>
2025-06-12 03:15:08 +0700

    Menambahkan libs toml

---
+++ app/src/main/java/com/fionasiregar0032/eventvault/model/Event.kt
@@ -0,0 +1,9 @@
+package com.fionasiregar0032.eventvault.model
+
+data class Event(
+    val id: Int,
+    val nama_kegiatan: String,
+    val deskripsi_kegiatan: String,
+    val tanggal_kegiatan: String,
+    val imageId: String
+)
--- app/src/main/java/com/fionasiregar0032/eventvault/network/EventApiService.kt
+++ app/src/main/java/com/fionasiregar0032/eventvault/network/EventApiService.kt
@@ -1,19 +1,26 @@
 package com.fionasiregar0032.eventvault.network
 
+import com.fionasiregar0032.eventvault.model.Event
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
 private const val BASE_URL ="https://store.sthresearch.site/"
 
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
 private val retrofit = Retrofit.Builder()
-    .addConverterFactory(ScalarsConverterFactory.create())
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
     .baseUrl(BASE_URL)
     .build()
 
 interface EventApiService {
     @GET("vault_event.php")
-    suspend fun getEvent(): String
+    suspend fun getEvent(): List<Event>
 }
 
 object EventApi {
