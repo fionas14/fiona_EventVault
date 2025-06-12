e7408784f0b19c2dc3ae331298b45fb34a242c5c
fionas14 <siregarfiona90@gamil.com>
2025-06-12 11:24:52 +0700

    Membuat indikator loading image

--- app/src/main/java/com/fionasiregar0032/eventvault/ui/screen/MainScreen.kt
+++ app/src/main/java/com/fionasiregar0032/eventvault/ui/screen/MainScreen.kt
@@ -24,6 +24,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -101,6 +102,7 @@ fun ScreenContent(modifier: Modifier = Modifier) {
                 .build(),
             contentDescription = "Gambar ${event.nama_kegiatan}",
             contentScale = ContentScale.Crop,
+            placeholder = painterResource(R.drawable.loading_img),
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(4.dp)
