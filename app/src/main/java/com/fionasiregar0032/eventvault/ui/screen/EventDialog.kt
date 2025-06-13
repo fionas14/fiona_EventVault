8026d63352b5b398bb86ed97b7d60ed1d05e68ac
fionas14 <siregarfiona90@gamil.com>
2025-06-13 23:28:15 +0700

    selesaiii

--- app/src/main/java/com/fionasiregar0032/eventvault/ui/screen/EventDialog.kt
+++ app/src/main/java/com/fionasiregar0032/eventvault/ui/screen/EventDialog.kt
@@ -22,11 +22,9 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.asImageBitmap
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardCapitalization
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
